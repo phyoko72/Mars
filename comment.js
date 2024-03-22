5afef79ebaa16f64cca53f0f28fae4aae1cafc66
phyoko <pkkoedu777@gmail.com>
2024-03-22 12:32:14 +0630

    added printCmt Function

--- comment.js
+++ comment.js
@@ -1,2 +1,8 @@
 const cmt = "Commented by Phoe La Pyae"
 console.log(cmt)
+
+function printCmt(comment) {
+    const [first, ...others] = comment.split("")
+    return first.toUpperCase().concat(others.join(""))
+}
+console.log(printCmt("canada"))
